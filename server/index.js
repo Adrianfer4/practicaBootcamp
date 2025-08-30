4bb61dd6a10928735c65037e6cbd915d68202179
Adrianfer4 <yerssondereck@gmail.com>
2025-08-30 15:01:22 -0500

    feat: agregar rutas y funcionalidad para gestión de usuarios con React y Express

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import route from "./routes/userRoutes.js";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 app.use(bodyParser.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017/mern";
